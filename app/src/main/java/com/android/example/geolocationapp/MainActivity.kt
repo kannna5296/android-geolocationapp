776cb2e0bd95df513316a0d91ca2a4b873c296bb
kannna5296 <kannnahacks5296@gmail.com>
2022-12-07 08:29:02 +0900

    "地図表示"Listener追加

--- app/src/main/java/com/android/example/geolocationapp/MainActivity.kt
+++ app/src/main/java/com/android/example/geolocationapp/MainActivity.kt
@@ -9,9 +9,22 @@ import android.widget.EditText
 import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
+
+    private var _latitude = 0.0
+    private var _longitude = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onMapShowCurrentButtonClick(view: View) {
+        val uriStr = "geo:${_latitude},${_longitude}"
+
+        val uri = Uri.parse(uriStr)
+
+        val intent = Intent(Intent.ACTION_VIEW,uri)
+        startActivity(intent)
     }
 
     fun onMapSearchButtonClick(view: View) {
